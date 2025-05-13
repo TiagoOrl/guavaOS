5c18d8bafdf7bf80a6f9a629a3a1dca64edd744f
Tiago <tiago_orlando@yahoo.com>
2025-05-13 20:26:19 -0300

    added bios print

--- src/main.asm
+++ src/main.asm
@@ -4,12 +4,46 @@ BITS 16     ; 16 bits mode
 
 
 main:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov sp, 0x7C00 ; start the stack from this location
+    mov si, os_boot_msg
+    call print
     HLT
 
 
 
 halt:
     JMP halt
+
+
+print:
+    push si
+    push ax
+    push bx
+
+print_loop:
+    LODSB   ; loads single byte from current location in SI register and puts in AL register
+    OR al, al
+    JZ done_print
+
+    mov ah, 0x0E    ; interrupt to print a character to the screen
+    mov bh, 0       ; page number argument
+    int 0x10        ; video interrupt
+
+    JMP print_loop
+
+done_print:
+    pop bx
+    pop ax
+    pop si
+    ret
+
+
+os_boot_msg: DB 'Welcome to Guava OS', 0x0D, 0x0A, 0
 
 ;
 TIMES 510-($-$$) DB 0   ; write 0 until byte 510
